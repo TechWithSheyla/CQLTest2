4d8505fe5df9192a428824441291e826e592e67c
Sheynnie05 <Sheynnie05@users.noreply.github.com>
2024-04-19 21:49:53 +0000

    ✅ Ready to clone and code.

---
+++ cqltest2/__main__.py
@@ -0,0 +1,6 @@
+"""Entry point for cqltest2."""
+
+from cqltest2.cli import main  # pragma: no cover
+
+if __name__ == "__main__":  # pragma: no cover
+    main()
--- tests/test_base.py
+++ tests/test_base.py
@@ -1,5 +1,5 @@
-from project_name.base import NAME
+from cqltest2.base import NAME
 
 
 def test_base():
-    assert NAME == "project_name"
+    assert NAME == "cqltest2"
